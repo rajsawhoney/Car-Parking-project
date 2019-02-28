5478dfe1259362ac802cd9a2740eb56ea9d34225
Razz Sawhoney <46258478+rajsawhoney@users.noreply.github.com>
2019-03-01 02:04:29 +0545

    Add files via upload

---
+++ CarClass.h
@@ -0,0 +1,43 @@
+//This is the store house of this program Car Parking
+
+#include <ctime>            //used for getting local date and time
+#include "timeConversion.h" //user defined header file to convert date and time in seconds into date and time format
+#include "date_time.h"      //user defined header file for getting date and time manually
+
+class ParkingLotControl
+{
+  private:
+    //For manual date and time
+    Time entry_time, exit_time; //Created two objects of class Time
+    Date entry_date, exit_date; //Created two objects of class Date
+
+    //For system date and time
+    string Entry, Exit;
+
+    string carId;
+    string name;
+    float Pcharge;
+    
+
+  public:
+    static int total_car_parked;    
+    bool entryStatus = false, exitStatus = false;
+    bool isSystem = false; //to work a/c to date and time mode
+    bool usingSystem = false;
+    bool isLotEmpty=true;
+    void get_entry();
+    void putcarId(string Id);
+    double checkTime();
+    void get_exit();
+    int checkDate();
+    float CheckTime();
+    void calCharge();
+    void showTotalTime();
+    void showTotalCarParked();
+    void showCharge();
+    void removeCar();
+    void operator++();
+    void operator--();
+    static int getTotalCar();
+    string getCarId();
+} c; //two class obj created
